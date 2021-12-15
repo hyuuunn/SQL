c724aa02431303c326de16f0c9f2684a672fe67d
Sunghyun Jung <93960513+hyuuunn@users.noreply.github.com>
2021-12-15 20:50:15 +0900

    Create 3-2_aggregate query.sql

---
+++ 3-2_aggregate query.sql
@@ -0,0 +1,281 @@
+-- 4-2. 집합쿼리 - 집합연산자 
+-- A 집합 
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ ORDER BY job_id;
+ 
+SELECT DISTINCT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ ORDER BY job_id;
+-- AD_ASST, IT_PROG, PU_CLERK, SH_CLERK, ST_CLERK 
+
+-- B 집합    
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;
+-- IT_PROG, MK_REP, ST_MAN 
+ 
+-- UNION 
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ UNION 
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;
+ 
+ 
+-- UNION 오류 -- 결과 열 수 불일치 
+SELECT job_id, salary
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ UNION 
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;
+ 
+ 
+-- UNION 오류 -- 결과 열의 데이텨형 불일치 
+SELECT job_id, salary
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ UNION 
+SELECT job_id, phone_number
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id; 
+ 
+
+-- UNION 오류 -- 문장오류는 없으나 의미상 오류인 쿼리 
+SELECT job_id, salary
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ UNION 
+SELECT job_id, department_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;  
+ 
+ 
+-- UNION ALL
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ UNION ALL
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id; 
+ 
+ 
+-- INTERSECT 
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ INTERSECT
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;  
+ 
+
+-- MINUS
+-- A - B
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ MINUS
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000
+ ORDER BY job_id;  
+ 
+-- B - A 
+SELECT job_id
+  FROM employees
+ WHERE 1=1   
+   AND salary BETWEEN 5001 AND 6000 
+ MINUS
+SELECT job_id
+  FROM employees
+ WHERE 1=1
+   AND salary BETWEEN 2000 and 5000
+ ORDER BY job_id;
+
+
+-- 집계 쿼리 실습 
+-- (1) 급여가 10000 이상인 사원의 평균 급여를 구하라
+SELECT AVG(salary)
+  FROM employees
+ WHERE salary >= 10000;
+ 
+SELECT ROUND(AVG(salary), 0)
+  FROM employees
+ WHERE salary >= 10000;
+
+-- (2) 입사 월별 사원수를 구하라
+SELECT TO_CHAR(hire_date, 'MM'), COUNT(*)
+  FROM employees
+GROUP BY TO_CHAR(hire_date, 'MM')
+ORDER BY 1;
+
+-- 요일별 입사 사원수는? 
+SELECT TO_CHAR(hire_date, 'day'), COUNT(*)
+  FROM employees
+GROUP BY TO_CHAR(hire_date, 'day')
+ORDER BY 1;
+
+
+-- (3) 이름이 동일한 사원과 동일인 수를 구하라
+SELECT  *
+  FROM employees
+ORDER BY first_name;
+
+SELECT first_name
+      ,COUNT(*)
+  FROM employees
+ GROUP BY first_name
+ ORDER BY first_name;
+ 
+ 
+SELECT first_name
+      ,COUNT(*)
+  FROM employees
+ GROUP BY first_name
+ HAVING COUNT(*) > 1
+ ORDER BY first_name;
+ 
+
+  
+-- 집합연산자 활용
+-- 1.예산 대비 실적 
+create table budget_table (
+     yearmon      VARCHAR2(6),
+     budget_amt   NUMBER     );
+     
+INSERT INTO budget_table values('201901', 1000);   
+INSERT INTO budget_table values('201902', 2000);   
+INSERT INTO budget_table values('201903', 1500);   
+INSERT INTO budget_table values('201904', 3000);   
+INSERT INTO budget_table values('201905', 1050);   
+
+create table sale_table (
+     yearmon      VARCHAR2(6),
+     sale_amt     NUMBER     );
+     
+INSERT INTO sale_table values('201901', 900);   
+INSERT INTO sale_table values('201902', 2000);   
+INSERT INTO sale_table values('201903', 1000);   
+INSERT INTO sale_table values('201904', 3100);   
+INSERT INTO sale_table values('201905', 800);   
+
+SELECT yearmon, budget_amt, 0 sale_amt
+  FROM budget_table
+ UNION 
+SELECT yearmon, 0 budget_amt, sale_amt
+  FROM sale_table;
+  
+SELECT yearmon, 
+       SUM(budget_amt) budget, 
+       SUM(sale_amt) sale,
+       ROUND(SUM(sale_amt) / SUM(budget_amt),2) * 100 rates
+  FROM ( SELECT yearmon, budget_amt, 0 sale_amt
+                FROM budget_table
+                UNION 
+                SELECT yearmon, 0 budget_amt, sale_amt
+                FROM sale_table
+              )   
+  GROUP BY yearmon
+  ORDER BY 1;
+
+
+-- 컬럼을 로우로 
+CREATE TABLE test_score (
+    years     VARCHAR2(4),
+    gubun     VARCHAR2(20),
+    korean    NUMBER,
+    english   NUMBER,
+    math      NUMBER );
+    
+INSERT INTO test_score VALUES ('2019', '중간고사', 92, 87, 67);
+INSERT INTO test_score VALUES ('2019', '기말고사', 88, 80, 91);
+
+
+SELECT years, gubun, '국어' subject, korean score FROM test_score
+UNION ALL
+SELECT years, gubun, '영어' subject, english score FROM test_score
+UNION ALL
+SELECT years, gubun, '수학' subject, math score FROM test_score
+ORDER BY 2 desc;
+
+-- INTERSECT
+SELECT state_province dup_loc_name
+  FROM locations
+INTERSECT
+SELECT city
+  FROM locations
+ORDER BY 1;  
+
+SELECT state_province, city
+  FROM locations 
+ WHERE state_province = city
+ORDER BY 1;
+
+-- 보너스 문제
+create table GROUPBYMULTIPLY (
+  department_name  VARCHAR2(100),
+  num_data         NUMBER 
+);
+
+insert into groupbymultiply values ('dept1', 10);
+insert into groupbymultiply values ('dept1', 20);
+insert into groupbymultiply values ('dept1', 30);
+insert into groupbymultiply values ('dept2', 5);
+insert into groupbymultiply values ('dept2', 7);
+insert into groupbymultiply values ('dept2', 40);
+insert into groupbymultiply values ('dept3', 69);
+insert into groupbymultiply values ('dept3', 71);
+insert into groupbymultiply values ('dept3', 12);
+
+commit;
+
+
+SELECT *
+FROM groupbymultiply;
+
+
+SELECT department_name,
+        SUM(num_data)
+FROM groupbymultiply
+GROUP BY department_name
+ORDER BY 1 ;
+
+
+SELECT department_name
+      ,EXP(SUM(LN(num_data))) multiply_result
+FROM groupbymultiply
+WHERE 1=1
+GROUP BY department_name
+ORDER BY 1;
