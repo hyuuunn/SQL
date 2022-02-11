8279f98b25e85ccb077f6e310839fdd0f2512267
Sunghyun Jung <93960513+hyuuunn@users.noreply.github.com>
2022-02-11 22:27:16 +0900

    Update 5-4_Q.sql

--- 5-4_Q.sql
+++ 5-4_Q.sql
@@ -11,3 +11,18 @@ SELECT a.employee_id,
     WHERE a.job_id = b.job_id ) avg_salary
  FROM employees a
  ORDER BY a.employee_id;
+
+-- 2. 사번, 사원명, 급여, 직급(job_id), 직급별 평균 급여 구하기 – 인라인 뷰 사용
+
+SELECT a.employee_id,
+  a.first_name || ' ' || a.last_name emp_name,
+  a.job_id,
+  a.salary,
+  b.avg_salary
+ FROM employees a,
+  ( SELECT job_id,
+      ROUND(AVG(salary)) avg_salary
+    FROM employees
+   GROUP BY job_id) b
+  WHERE a.job_id = b.job_id
+  ORDER BY a.employee_id;
