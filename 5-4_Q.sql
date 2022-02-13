0bc79db18783293a093c187534154bebe6d9ac1c
Sunghyun Jung <93960513+hyuuunn@users.noreply.github.com>
2022-02-13 23:15:13 +0900

    Update 5-4_Q.sql

--- 5-4_Q.sql
+++ 5-4_Q.sql
@@ -39,3 +39,23 @@ SELECT employee_id, job_id, salary
                   FROM jobs b
                 WHERE a.job_id = b.job_id
                   AND a.salary = b.min_salary );
+
+-- 4. covid19 테이블을 사용해 월별, 대륙별, 국가별 감염수와 각 국가가 속한 대륙을 기준으로 감염수 비율을 구하는 쿼리를 작성하시오.
+
+SELECT a.months, a.continent, a.country, a.new_cases, b.continent_cases
+       ,DECODE(b.continent_cases, 0, 0, ROUND(a.new_cases / b.continent_cases * 100,2)) rates
+      FROM ( SELECT country, continent
+            ,TO_CHAR(dates, 'yyyy-mm') months
+            ,SUM(new_cases) new_cases
+          FROM COVID19_TEST
+        GROUP BY TO_CHAR(dates, 'yyyy-mm'), country, continent )a
+          ,( SELECT continent
+               ,TO_CHAR(dates, 'yyyy-mm') months
+               ,SUM(new_cases) continent_cases
+             FROM COVID19_TEST
+            GROUP BY TO_CHAR(dates, 'yyyy-mm'), continent
+           )b
+ WHERE a.months = b.months
+  AND a.continent = b.continent
+  AND a.new_cases > 0
+ ORDER BY 1, 2, 4 DESC ;
